563309d1637dd358f9660284972d94198db32397
Alexis <abonansea@algeiba.com>
2023-11-25 00:23:09 -0300

    Sexo data

---
+++ Datos/SexoDA.cs
@@ -0,0 +1,22 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class SexoDA
+    {
+        Entidades.Modelo context = new Entidades.Modelo();
+
+        public List<Sexo> getSexos()
+        {
+            var listaSexos = from r in context.Sexos
+                             select r;
+
+            return listaSexos.ToList();
+        }
+    }
+}
---
+++ Logica/SexoLOG.cs
@@ -0,0 +1,19 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class SexoLOG
+    {
+        Datos.SexoDA sexo = new Datos.SexoDA();
+
+        public List<Sexo> getSexos()
+        {
+            return sexo.getSexos();
+        }
+    }
+}
